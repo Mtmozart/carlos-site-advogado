c1a06dcd0ba008d7d8bc4f439e8a5fb1f8aa28ae
matheus mozart <mmsnborges@gmail.com>
2024-07-20 15:16:35 -0300

    finalizando o cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Cover } from '@/components/cover/cover';
-
+import styles from './styles.module.scss';
 export default function Home() {
   return (
     <>
-      <Cover />
+      <main className={styles.main_container}>
+        <Cover />
+      </main>
     </>
   );
 }
--- src/components/cover/cover.tsx
+++ src/components/cover/cover.tsx
@@ -1,3 +1,20 @@
+import styles from './cover.module.scss';
+import { FaWhatsapp } from 'react-icons/fa';
 export const Cover = () => {
-  return <h1>Hellor world</h1>;
+  return (
+    <section className={styles.cover__container}>
+      <div className={styles.cover__content}>
+        <h1>Carlos Pontes Advogado</h1>
+        <h2>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, illo.
+          Adipisci doloribus saepe nulla voluptatem eaque ratione. Ipsam odit
+          ducimus expedita in necessitatibus numquam reprehenderit, enim totam
+          vel dicta voluptatum?
+        </h2>
+        <a className={styles.call_to_action}>
+          Contato <FaWhatsapp />
+        </a>
+      </div>
+    </section>
+  );
 };
